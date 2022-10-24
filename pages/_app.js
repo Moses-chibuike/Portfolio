bfe48fc0c44a3fd8e87467aff06ba64a094b2ca4
zeusmoney <ezechukwuchibuikemoses@gmail.com>
2022-10-24 16:51:17 +0100

    initial commit

---
+++ pages/_app.js
@@ -0,0 +1,11 @@
+import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'remixicon/fonts/remixicon.css'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
